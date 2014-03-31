a217c1150a00518ce672b75b9e84c30de80fb27b
Boehm Tomi S <tboehm@da1-cs-b221-23.helsinki.fi>
2014-03-31 13:21:28 +0300

    kirjaDAO muutos

--- viitemanageri/src/main/java/viitemanageri/Kirja.java
+++ viitemanageri/src/main/java/viitemanageri/Kirja.java
@@ -19,5 +19,37 @@ public class Kirja implements Viite {
         this.vuosi = vuosi;
         this.julkaisija = julkaisija;
     }      
+
+    public String getTekija() {
+        return tekija;
+    }
+
+    public void setTekija(String tekija) {
+        this.tekija = tekija;
+    }
+
+    public String getTeoksenNimi() {
+        return teoksenNimi;
+    }
+
+    public void setTeoksenNimi(String teoksenNimi) {
+        this.teoksenNimi = teoksenNimi;
+    }
+
+    public int getVuosi() {
+        return vuosi;
+    }
+
+    public void setVuosi(int vuosi) {
+        this.vuosi = vuosi;
+    }
+
+    public String getJulkaisija() {
+        return julkaisija;
+    }
+
+    public void setJulkaisija(String julkaisija) {
+        this.julkaisija = julkaisija;
+    }
     
 }
