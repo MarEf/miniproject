04cf49dee5e7c11a47896351adc460959f6ac9a0
Maria Efimvoa <maria.efimova93@gmail.com>
2014-04-06 17:49:43 +0300

    luokat artikkelille ja inprolle luotu

---
+++ viitemanageri/src/main/java/viitemanageri/viitteet/Inproceedins.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package viitemanageri.viitteet;
+
+import java.util.List;
+
+/**
+ *
+ * @author maria
+ */
+public class Inproceedins implements Viite{
+
+    @Override
+    public List<Arvo> palautaKaikkiArvot() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getTunnus() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
